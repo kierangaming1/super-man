ab4b18ef298a836df97e7985d3cb69f346d93f3f
kierangaming1 <34714843+kierangaming1@users.noreply.github.com>
2018-07-27 11:21:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,12 +14,23 @@ client.on('ready', () => {
 
 client.on('message', message => {
 
-    if (message.content === 'ping') {
+    if (message.content === 'info') {
 
-       message.reply('pong');
+       message.reply('if you need help with this bot anyway please go to https://discord.gg/QeCpKKu But This bot was created by kierangaming1#8848 And CO-Created by HexDev#0001 DOnt tell him I told you he is the co-creator Anyways  We are cuently trying to get more commands working Anways we are ***Sorry This bot has not got alot of commands at this time be wait for commands*** **Beta**
+');
 
-       }
+       } else 
+                     
+      
+ if(message.content.startsWith("ping")) {
+            message.channel.send(new Date().getTime() - message.createdTimestamp + " ms");  
+       } else 
+     if (message.content === 'help') {
 
+       message.reply('Ping : sees if the bots running fine , help: This screen , say: says what you said , !kick : kicks a player (you cannot add a reason yet ) , !ban : bans a player (you cannot add a reason yet)'
+        } else
+                     
+                     
 });
 
  
