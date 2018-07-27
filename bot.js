7d0dc182316f9ff43defe20aba4168801d90d107
kierangaming1 <34714843+kierangaming1@users.noreply.github.com>
2018-07-27 13:09:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,26 +2,30 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
+ 
 
-client.on('ready',() => {
-	console.log('I\'m Online\nI\'m Online');
+client.on('ready', () => {
+
+    console.log('I am ready!');
+
 });
 
-var prefix = "~"
-client.on('message', message => {
-	if (message.author === client.user) return;
-	if (message.content.startsWith(prefix + 'info')) {
-		message.channel.sendMessage('if you need help with this bot anyway please go to https://discord.gg/QeCpKKu But This bot was created by kierangaming1#8848 And CO-Created by HexDev#0001 DOnt tell him I told you he is the co-creator Anyways  We are cuently trying to get more commands working Anways we are ***Sorry This bot has not got alot of commands at this time be wait for commands*** **Beta**');
-	} else 
  
+
+client.on('message', message => {
+ 
+
+    if (message.content === '!info') {
+
+       message.reply('if you need help with this bot anyway please go to https://discord.gg/QeCpKKu But This bot was created by kierangaming1#8848 And CO-Created by HexDev#0001 DOnt tell him I told you he is the co-creator Anyways  We are cuently trying to get more commands working Anways we are ***Sorry This bot has not got alot of commands at this time be wait for commands*** **Beta**');
+
+       } else 
                      
       
- if (message.author === client.user) return;
-	if (message.content.startsWith(prefix +'ping')) {
+ if(message.content.startsWith("!ping")) {
             message.channel.send(new Date().getTime() - message.createdTimestamp + " ms");  
        } else 
-     if (message.author === client.user) return;
-	if (message.content.startsWith(prefix + 'help') {
+     if (message.content === '!help') {
 
        message.reply('Ping : sees if the bots running fine , help: This screen , say: says what you said , !kick : kicks a player (you cannot add a reason yet ) , !ban : bans a player (you cannot add a reason yet)');
         } 
