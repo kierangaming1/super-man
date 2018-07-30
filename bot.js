ac9e7571bc7accb1b6340f92d4398d4262cc07e7
kierangaming1 <34714843+kierangaming1@users.noreply.github.com>
2018-07-30 17:06:29 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,6 +15,23 @@ client.on('ready', () => {
 client.on('message', message => {
   // Ignore messages that aren't from a guild
   if (!message.guild) return;
+  
+if (message.content === '!info') {
+
+       message.reply('if you need help with this bot anyway please go to https://discord.gg/QeCpKKu But This bot was created by kierangaming1#8848 And CO-Created by HexDev#0001 DOnt tell him I told you he is the co-creator Anyways  We are cuently trying to get more commands working Anways we are ***Sorry This bot has not got alot of commands at this time be wait for commands*** **Beta**');
+
+       } else 
+                     
+      
+ if(message.content.startsWith("!ping")) {
+            message.channel.send(new Date().getTime() - message.createdTimestamp + " ms");  
+       } else 
+     if (message.content === '!help') {
+
+       message.reply('Ping : sees if the bots running fine , help: This screen , !kick : kicks a user');
+        } else 
+  
+                     
 
   // If the message content starts with "!kick"
   if (message.content.startsWith('!kick')) {
