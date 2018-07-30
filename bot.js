f89a29524ad0ef06d81bd8bc611215928f9ca6e3
kierangaming1 <34714843+kierangaming1@users.noreply.github.com>
2018-07-30 17:00:44 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,39 +1,58 @@
+// Import the discord.js module
 const Discord = require('discord.js');
 
+// Create an instance of a Discord client
 const client = new Discord.Client();
 
- 
-
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
 client.on('ready', () => {
-
-    console.log('I am ready!');
-
+  console.log('I am ready!');
 });
-
- 
 
 client.on('message', message => {
- 
+  // Ignore messages that aren't from a guild
+  if (!message.guild) return;
 
-    if (message.content === '!info') {
-
-       message.reply('if you need help with this bot anyway please go to https://discord.gg/QeCpKKu But This bot was created by kierangaming1#8848 And CO-Created by HexDev#0001 DOnt tell him I told you he is the co-creator Anyways  We are cuently trying to get more commands working Anways we are ***Sorry This bot has not got alot of commands at this time be wait for commands*** **Beta**');
-
-       } else 
-                     
-      
- if(message.content.startsWith("!ping")) {
-            message.channel.send(new Date().getTime() - message.createdTimestamp + " ms");  
-       } else 
-     if (message.content === '!help') {
-
-       message.reply('Ping : sees if the bots running fine , help: This screen ,');
-        } 
-  
-                     
-});
-
- 
+  // If the message content starts with "!kick"
+  if (message.content.startsWith('!kick')) {
+    // Assuming we mention someone in the message, this will return the user
+    // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
+    const user = message.mentions.users.first();
+    // If we have a user mentioned
+    if (user) {
+      // Now we get the member from the user
+      const member = message.guild.member(user);
+      // If the member is in the guild
+      if (member) {
+        /**
+         * Kick the member
+         * Make sure you run this on a member, not a user!
+         * There are big differences between a user and a member
+         */
+        member.kick('Optional reason that will display in the audit logs').then(() => {
+          // We let the message author know we were able to kick the person
+          message.reply(`Successfully kicked ${user.tag}`);
+        }).catch(err => {
+          // An error happened
+          // This is generally due to the bot not being able to kick the member,
+          // either due to missing permissions or role hierarchy
+          message.reply('I was unable to kick the member');
+          // Log the error
+          console.error(err);
+        });
+      } else {
+        // The mentioned user isn't in this guild
+        message.reply('That user isn\'t in this guild!');
+      }
+    // Otherwise, if no user was mentioned
+    } else {
+      message.reply('You didn\'t mention the user to kick!');
+    }
+  } 
+ });
 
 // THIS  MUST  BE  THIS  WAY
 
